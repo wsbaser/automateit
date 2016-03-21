c4f5a3d142c5c48e6e32191f5a9f575b5ec1df23
Alexey Volkov <aleksey.v@livetex.ru>
2016-03-21 18:35:39 +0300

    get chatserver endpoint from balancer

---
+++ selenium.widgets-ui-3.0/api/ChatServer/Balancer.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net;
+using System.Xml;
+using System.Xml.XPath;
+using NUnit.Framework;
+
+namespace selenium.widget.v3.api.ChatServer
+{
+    public class Balancer
+    {
+        public string Host { get; }
+
+        public Balancer(string host)
+        {
+            Host = host;
+        }
+
+        public string RequestChatServerEndpoint(string login)
+        {
+            //"http://balancer-cloud.global.livetex/?"
+            var builder = new UriBuilder("http", Host, 80);
+            builder.Query = $@"sender=operator&action=getserver&login={login}&protocol=0.1.0";
+            using (var webClient = new WebClient())
+            {
+                string responseXml = webClient.DownloadString(builder.Uri);
+                return ReadEndPoint(responseXml);
+            }
+        }
+
+        private string ReadEndPoint(string balancerRresponseXml)
+        {
+            var doc = new XmlDocument();
+            doc.LoadXml(balancerRresponseXml);
+            var xPathNavigator = doc.CreateNavigator();
+            XPathNavigator ipNode = xPathNavigator.SelectSingleNode("//server_ip");
+            XPathNavigator portNode = xPathNavigator.SelectSingleNode("//server_port");
+            if (ipNode == null || portNode == null)
+                return null;
+            return BuildEndPointUrl(ipNode.Value, portNode.Value);
+        }
+
+        private string BuildEndPointUrl(string ip, string port)
+        {
+            return $@"ws://{ip}:{port}";
+        }
+    }
+
+    [TestFixture]
+    public class BalancerTest
+    {
+        [Test]
+        public void GetChatServerEndpoint()
+        {
+            // .Arrange
+            var balancer = new Balancer("balancer-cloud.global.livetex");
+            // .Act
+            string endPointUrl = balancer.RequestChatServerEndpoint("aleksey.v%40livetex.ru");
+            // .Assert
+            Assert.AreEqual(endPointUrl, "ws://action-1.unstable.livetex:19090");
+        }
+    }
+}
---
+++ selenium.widgets-ui-3.0/api/ChatServer/Operator.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using WebSocketSharp;
+
+namespace selenium.widget.v3.api.ChatServer
+{
+    public class Operator
+    {
+        public string BalancerUrl { get; }
+        public string Login { get; }
+        private WebSocket _webSocket;
+        private List<string> _messages; 
+
+        public Operator(string login, string balancerUrl)
+        {
+            BalancerUrl = balancerUrl;
+            Login = login;
+            _messages = new List<string>();
+            Connect();
+        }
+
+        //        operatorName=Admin
+        //login = widgets.auto@yandex.ru
+        //  password = 1qaz2wsx3edc
+        //    customerCare = https://customer-care.env-03.testing-02/
+        //apiv2=apiv2.env-03.testing-02
+        //balancer=balancer.livetex.build1
+        //siteId = 10009394
+
+        private void Connect()
+        {
+            _webSocket = new WebSocket(BalancerUrl);
+            _webSocket.OnMessage += OnMessage;
+            try
+            {
+                _webSocket.Connect();
+            }
+            catch (Exception e)
+            {
+                throw new Exception($@"Unable to connect to {BalancerUrl}", e);
+            }
+        }
+
+        private void OnMessage(object sender, MessageEventArgs e)
+        {
+            _messages.Add(e.Data);
+            Console.WriteLine("OnMessage: " + e.Data);
+        }
+
+        private void Send(Object obj)
+        {
+            _webSocket.Send(JsonConvert.SerializeObject(obj));
+        }
+
+        public void Authenticate(string password)
+        {
+            var request = new
+            {
+                request = "auth",
+                body = new
+                {
+                    client_version = "272",
+                    is_busy = false,
+                    os = new
+                    {
+                        arch = "x64",
+                        build = "3.9-1-amd64",
+                        name = "",
+                        type = "Linux",
+                        version = ""
+                    },
+                    protocol_version = "0.1.0",
+                    source = "operator",
+                    login = Login,
+                    password = password
+                }
+            };
+            Send(request);
+        }
+    }
+
+    [TestFixture]
+    public class OperatorTests {
+        [Test]
+        public void Authenticate()
+        {
+            // .Arrange
+            var _operator = new Operator("operator_for_chat@tt.tt", "ws://balancer.livetex.build1xxx");
+            // .Act
+            _operator.Authenticate("1qaz2wsx");
+            // .Assert
+        }
+    }
+}
